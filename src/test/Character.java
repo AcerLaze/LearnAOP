75eff624c6d0c4bf07a921d9102448207aae59e1
AcerLazerRazerTazer <soulcommander12@gmail.com>
2019-10-17 12:22:49 +0700

    third commit

--- src/test/Character.java
+++ src/test/Character.java
@@ -2,8 +2,87 @@ package test;
 
 public class Character {
 
+	private String name;
+	private double strength;
+	private double intelligence;
+	private double hp;
+	private double defense;
+	private String type;
+	
 	public Character(){}
 	
-	public Character(String name){}
+	public Character(String name){this.name = name;}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public double getIntelligence() {
+		return intelligence;
+	}
+
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+
+	public double getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
+	
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+	
+	public double getDefense() {
+		return defense;
+	}
+
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+	public double attack(Character enemy){
+		
+		if(strength > intelligence) return (strength - enemy.getDefense());
+		else if(intelligence > strength) return (intelligence);
+		else return (((strength - enemy.getDefense())*intelligence)/2);
+		
+	}
+	
+	public void receivedDmg(double dmg){
+		
+		hp -= dmg;
+		
+	}
+
+	public double atkModifier(Character enemy){
+		
+		return 1;
+		
+	}
 
 }
--- src/test/FireType.java
+++ src/test/FireType.java
@@ -1,10 +1,20 @@
 package test;
 
 public class FireType extends Character{
-
+	
 	public FireType(){
 		
 		super("Mokou");
+		this.setType("Fire");
+		
+	}
+	
+	public double atkModifier(Character enemy){
+		
+		if(enemy.getType() == "Water") return 0.5;
+		else if (enemy.getType() == "Earth") return 2;
+		else if (enemy.getType() == "Fire") return 0;
+		else return 1;
 		
 	}
 	
